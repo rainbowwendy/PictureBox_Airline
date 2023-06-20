cde406f45860003cf4c5f78225799059bebe5c6a
Wendy_Lee <ddang727@hotmail.com>
2023-06-20 12:26:49 -0700

    Add project files.

---
+++ PictureBox_Airline/Form1.Designer.vb
@@ -0,0 +1,142 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Business = New TabControl()
+        TabPage1 = New TabPage()
+        TabPage2 = New TabPage()
+        Label3 = New Label()
+        Label4 = New Label()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        Label1 = New Label()
+        Business.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Business
+        ' 
+        Business.Controls.Add(TabPage1)
+        Business.Controls.Add(TabPage2)
+        Business.Location = New Point(12, 29)
+        Business.Name = "Business"
+        Business.SelectedIndex = 0
+        Business.Size = New Size(1109, 682)
+        Business.TabIndex = 0
+        ' 
+        ' TabPage1
+        ' 
+        TabPage1.AllowDrop = True
+        TabPage1.AutoScroll = True
+        TabPage1.BackColor = Color.Transparent
+        TabPage1.ForeColor = SystemColors.Control
+        TabPage1.Location = New Point(4, 29)
+        TabPage1.Name = "TabPage1"
+        TabPage1.Padding = New Padding(3)
+        TabPage1.Size = New Size(1101, 649)
+        TabPage1.TabIndex = 0
+        TabPage1.Text = "Business" ' 
+        ' TabPage2
+        ' 
+        TabPage2.AutoScroll = True
+        TabPage2.Location = New Point(4, 29)
+        TabPage2.Name = "TabPage2"
+        TabPage2.Padding = New Padding(3)
+        TabPage2.Size = New Size(1101, 649)
+        TabPage2.TabIndex = 1
+        TabPage2.Text = "Economy"
+        TabPage2.UseVisualStyleBackColor = True
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.ForeColor = SystemColors.HotTrack
+        Label3.Location = New Point(1139, 225)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(80, 20)
+        Label3.TabIndex = 1
+        Label3.Text = "First Name" ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.ForeColor = SystemColors.HotTrack
+        Label4.Location = New Point(1139, 269)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(79, 20)
+        Label4.TabIndex = 2
+        Label4.Text = "Last Name" ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(1221, 217)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(144, 27)
+        TextBox3.TabIndex = 3
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(1221, 266)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(144, 27)
+        TextBox4.TabIndex = 4
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.ForeColor = SystemColors.HotTrack
+        Label1.Location = New Point(1139, 177)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(281, 20)
+        Label1.TabIndex = 6
+        Label1.Text = "Please fill your name and select your seat" ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8.0F, 20.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ActiveCaption
+        ClientSize = New Size(1452, 769)
+        Controls.Add(Label1)
+        Controls.Add(TextBox4)
+        Controls.Add(TextBox3)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Business)
+        ForeColor = SystemColors.ActiveCaptionText
+        Name = "Form1"
+        Text = "Form1"
+        TransparencyKey = Color.MistyRose
+        Business.ResumeLayout(False)
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Business As TabControl
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents VScrollBar1 As VScrollBar
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ PictureBox_Airline/Form1.vb
@@ -0,0 +1,153 @@
+﻿Imports System.Reflection
+
+Public Class Form1
+    Dim BizClass(20) As PictureBox      'Biz = Business
+    Dim BizClassNames(20) As RichTextBox
+    Dim EcoClass(100) As PictureBox     'Eco = Economy
+    Dim EcoClassNames(100) As RichTextBox
+    Dim NameBoxes(2) As TextBox
+    Dim Names(2) As String 'Names(2)=> make 2 arrays
+    'Dim myImageLocationPrefix As String = "C:\Repo\VB\PictureBox_Airline\PictureBox_Airline\bin\Debug\net6.0-windows"
+    Dim EmptySeatImg As String = "./airlineSeatEmpty.png" '이미지가 아닌 Text로 받으로 string type.
+    Dim FullSeatImg As String = "./airlineSeat.png"
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        '------------ Business Class  
+
+        Dim xLocation As Integer = 10 'Form1의 왼쪽에서부터 시작하는데 10pixel의 space를 두고 시작
+        Dim yLocation As Integer = 10 'Form1의 위에서부터 시작하는데 10pixel의 space를 두고 시작
+        Names(0) = "First Name"
+        Names(1) = "Last Name"
+
+        For index = 0 To 19 'loop for 20 seats
+            'Mybox is where we are instantiating the PictureBoxObject
+            BizClass(index) = New PictureBox() With { 'New PictureBox: PIctureBox라는 큰 덩어리의 것을 새로 부른다.
+            .ImageLocation = EmptySeatImg,
+            .Size = New Size(200, 200),
+            .Location = New Point(xLocation, yLocation),
+            .Name = "BizPictureBox" + index.ToString(),
+            .Visible = True,
+            .Enabled = True,
+            .Cursor = Cursors.Hand
+            } ' Inside the curly braces is where we add the properties
+            BizClassNames(index) = New RichTextBox() With {
+            .Size = New Size(200, 100), 'New = memory allocator, Size = a type like integer, point, string.
+            .Location = New Point(xLocation, yLocation + 205),
+            .Name = "RichTextBox" + index.ToString(),
+            .Visible = True,
+            .Enabled = True,
+            .Cursor = Cursors.Hand
+            }
+
+            AddHandler BizClass(index).Click, AddressOf PictureBoxClick 'bizclass가 클릭되면, PictureBoxClick이라는 함수를 호출해라
+            'Me.Controls.Add(BusinessClass(index)) 'Me is a reference to the form1, to translate it would be form1.controls.Add(MyBox). This adds MyBox to form1
+
+            TabPage1.Controls.Add(BizClass(index))
+            TabPage1.Controls.Add(BizClassNames(index))
+
+            xLocation += 210
+            If (index + 1) Mod 4 = 0 Then '이미지가 5개가 되면 다음칸으로 가도록 하는 코드
+                xLocation = 10
+                yLocation += 310
+            End If
+        Next
+
+        '------------Economy Class
+
+        xLocation = 10
+        yLocation = 10
+
+        For index = 0 To 99 'For loop 100 seats
+            EcoClass(index) = New PictureBox() With {
+            .ImageLocation = EmptySeatImg,
+            .Size = New Size(200, 200),
+            .Location = New Point(xLocation, yLocation),
+            .Name = "EcoPictureBox" + index.ToString(),
+            .Visible = True,
+            .Enabled = True,
+            .Cursor = Cursors.Hand
+            }
+            EcoClassNames(index) = New RichTextBox() With {
+            .Size = New Size(200, 100),
+            .Location = New Point(xLocation, yLocation + 205),
+            .Name = "RichTextBox" + index.ToString(),
+            .Visible = True,
+            .Enabled = True,
+            .Cursor = Cursors.Hand
+            }
+
+            AddHandler EcoClass(index).Click, AddressOf PictureBoxClick
+
+            TabPage2.Controls.Add(EcoClass(index))
+            TabPage2.Controls.Add(EcoClassNames(index))
+
+            xLocation += 210
+            If (index + 1) Mod 4 = 0 Then
+                xLocation = 10
+                yLocation += 310
+            End If
+        Next
+    End Sub
+
+    Private Sub PictureBoxClick(sender As Object, e As EventArgs) 'PictureBoxClick: function, sender& e: parameter, Object& EventArgs: type
+        Dim PictureBoxClicked As PictureBox = sender
+        Dim PictureBoxIndex As Integer = Convert.ToInt32(PictureBoxClicked.Name.Substring(13)) '베열의 index는 무조건 숫자여야 해서 이름에서 글자를 빼고 String인 "1"을 타입변환(to integer)해서 PictureBoxIndex에 넣어 index로 사용. 배열은 덩어리기 때문에 인덱스를 사용하여 위치(주소)를 찾음
+        Dim BizOrEco As String = (PictureBoxClicked.Name.Substring(0, 3))
+        NameBoxes(0) = TextBox3
+        NameBoxes(1) = TextBox4
+
+        If BizOrEco = "Biz" Then
+            If (NameBoxes(0).Text = "" Or NameBoxes(1).Text = "") And BizClassNames(PictureBoxIndex).Text = "" Then
+                MessageBox.Show($"Please fill in {Names(0)} and {Names(1)}")
+                Return
+            End If
+        End If
+
+        If BizOrEco = "Eco" Then
+            If (NameBoxes(0).Text = "" Or NameBoxes(1).Text = "") And EcoClassNames(PictureBoxIndex).Text = "" Then
+                MessageBox.Show($"Please fill in {Names(0)} and {Names(1)}")
+                Return
+            End If
+        End If
+
+        If PictureBoxClicked.ImageLocation = EmptySeatImg And BizOrEco = "Biz" Then
+            PictureBoxClicked.ImageLocation = FullSeatImg
+            BizClassNames(PictureBoxIndex).Text = $"{Names(0)}: {NameBoxes(0).Text} {vbCrLf}{Names(1)}: {NameBoxes(1).Text} "
+            Return
+        End If
+
+        If PictureBoxClicked.ImageLocation = EmptySeatImg And BizOrEco = "Eco" Then
+            PictureBoxClicked.ImageLocation = FullSeatImg
+            EcoClassNames(PictureBoxIndex).Text = $"{Names(0)}: {NameBoxes(0).Text} {vbCrLf}{Names(1)}: {NameBoxes(1).Text} "
+            Return
+        End If
+
+        '--------- Cancellation
+
+        If PictureBoxClicked.ImageLocation = FullSeatImg And BizOrEco = "Biz" Then
+
+            Dim result As DialogResult = MessageBox.Show("Do you want to cancel your reservation?", "Please confirm", MessageBoxButtons.YesNo)
+            If result = DialogResult.Yes Then
+                PictureBoxClicked.ImageLocation = EmptySeatImg
+                BizClassNames(PictureBoxIndex).Text = ""
+                MessageBox.Show($"Your reservation has been cancelled.")
+            ElseIf result = DialogResult.No Then
+                MessageBox.Show($"Thank you for your reservation!")
+            End If
+            Return
+        End If
+
+        If PictureBoxClicked.ImageLocation = FullSeatImg And BizOrEco = "Eco" Then
+            Dim result As DialogResult = MessageBox.Show("Do you want to cancel your reservation?", "Please confirm", MessageBoxButtons.YesNo)
+            If result = DialogResult.Yes Then
+                PictureBoxClicked.ImageLocation = EmptySeatImg
+                EcoClassNames(PictureBoxIndex).Text = ""
+                MessageBox.Show($"Your reservation has been cancelled.")
+            ElseIf result = DialogResult.No Then
+                MessageBox.Show($"Thank you for your reservation!")
+            End If
+            Return
+        End If
+    End Sub
+End Class
